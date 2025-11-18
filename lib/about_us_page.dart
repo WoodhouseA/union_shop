1e0d074df8cf153c3e84de57d724dc93f7c233da
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-11-18 16:13:33 +0000

    Add static text for about us page

--- lib/about_us_page.dart
+++ lib/about_us_page.dart
@@ -17,6 +17,31 @@ class AboutUsPage extends StatelessWidget {
               'Welcome to the Union Shop!',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
+            SizedBox(height: 16),
+            Text(
+              'We’re dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'All online purchases are available for delivery or instore collection!',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don’t hesitate to contact us at hello@upsu.net.',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 24),
+            Text(
+              'Happy shopping!',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'The Union Shop & Reception Team',
+              style: TextStyle(fontSize: 16),
+            ),
           ],
         ),
       ),
