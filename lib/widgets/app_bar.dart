7c2bcc14f574bc8f7a733acf96d1f6b03c89b2cb
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-11-22 14:43:10 +0000

    Refactor CustomAppBar to use StatefulWidget for menu toggle functionality

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
   const CustomAppBar({super.key});
+
+  @override
+  State<CustomAppBar> createState() => _CustomAppBarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(100);
+}
+
+class _CustomAppBarState extends State<CustomAppBar> {
+  bool _isMenuOpen = false;
+
+  void _toggleMenu() {
+    setState(() {
+      _isMenuOpen = !_isMenuOpen;
+    });
+  }
 
   void navigateToHome(BuildContext context) {
     Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
@@ -13,85 +29,87 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      color: Colors.white,
-      child: Column(
-        children: [
-          // Top banner
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            color: const Color(0xFF4d2963),
-            child: const Text(
-              'UNIVERSITY OF PORTSMOUTH SHOP',
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white, fontSize: 16),
-            ),
-          ),
-          // Main header
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Row(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      navigateToHome(context);
-                    },
-                    child: Image.network(
-                      'https://shop.upsu.net/cdn/shop/files/upsu_300x300.png?v=1614735854',
-                      height: 18,
-                      fit: BoxFit.cover,
-                      errorBuilder: (context, error, stackTrace) {
-                        return Container(
-                          color: Colors.grey[300],
-                          width: 18,
-                          height: 18,
-                          child: const Center(
-                            child: Icon(Icons.image_not_supported,
-                                color: Colors.grey),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  const Spacer(),
-                  ConstrainedBox(
-                    constraints: const BoxConstraints(maxWidth: 600),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        AppBarButton(
-                          icon: Icons.search,
-                          onPressed: placeholderCallbackForButtons,
-                        ),
-                        AppBarButton(
-                          icon: Icons.person_outline,
-                          onPressed: placeholderCallbackForButtons,
-                        ),
-                        AppBarButton(
-                          icon: Icons.shopping_bag_outlined,
-                          onPressed: placeholderCallbackForButtons,
-                        ),
-                        AppBarButton(
-                          icon: Icons.menu,
-                          onPressed: placeholderCallbackForButtons,
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+    return Column(
+      children: [
+        Container(
+          height: 100,
+          color: Colors.white,
+          child: Column(
+            children: [
+              // Top banner
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                color: const Color(0xFF4d2963),
+                child: const Text(
+                  'UNIVERSITY OF PORTSMOUTH SHOP',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
               ),
-            ),
+              // Main header
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          navigateToHome(context);
+                        },
+                        child: Image.network(
+                          'https://shop.upsu.net/cdn/shop/files/upsu_300x300.png?v=1614735854',
+                          height: 18,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) {
+                            return Container(
+                              color: Colors.grey[300],
+                              width: 18,
+                              height: 18,
+                              child: const Center(
+                                child: Icon(Icons.image_not_supported,
+                                    color: Colors.grey),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      const Spacer(),
+                      ConstrainedBox(
+                        constraints: const BoxConstraints(maxWidth: 600),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            AppBarButton(
+                              icon: Icons.search,
+                              onPressed: placeholderCallbackForButtons,
+                            ),
+                            AppBarButton(
+                              icon: Icons.person_outline,
+                              onPressed: placeholderCallbackForButtons,
+                            ),
+                            AppBarButton(
+                              icon: Icons.shopping_bag_outlined,
+                              onPressed: placeholderCallbackForButtons,
+                            ),
+                            AppBarButton(
+                              icon: Icons.menu,
+                              onPressed: _toggleMenu,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        if (_isMenuOpen) const _MobileMenu(),
+      ],
     );
   }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(100);
 }
 
 class AppBarButton extends StatelessWidget {
