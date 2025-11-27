630d03776b0883eb19831681849f9d1fbc95bded
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-11-27 17:19:13 +0000

    Refactor CustomAppBar to accept onMenuPressed callback and streamline button handling

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:union_shop/widgets/mobile_menu.dart';
 
 class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
-  const CustomAppBar({super.key});
+  final VoidCallback onMenuPressed;
+  const CustomAppBar({super.key, required this.onMenuPressed});
 
   @override
   State<CustomAppBar> createState() => _CustomAppBarState();
@@ -12,14 +12,6 @@ class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
 }
 
 class _CustomAppBarState extends State<CustomAppBar> {
-  bool _isMenuOpen = false;
-
-  void _toggleMenu() {
-    setState(() {
-      _isMenuOpen = !_isMenuOpen;
-    });
-  }
-
   void navigateToHome(BuildContext context) {
     Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
   }
@@ -30,101 +22,83 @@ class _CustomAppBarState extends State<CustomAppBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      clipBehavior: Clip.none,
-      children: [
-        // The actual AppBar, with a fixed height.
-        Material(
-          elevation: 1.0,
-          child: Container(
-            height: 100,
-            color: Colors.white,
-            child: Column(
-              children: [
-                // Top banner
-                Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                  color: const Color(0xFF4d2963),
-                  child: const Text(
-                    'UNIVERSITY OF PORTSMOUTH SHOP',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white, fontSize: 16),
-                  ),
-                ),
-                // Main header
-                Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            navigateToHome(context);
-                          },
-                          child: Image.network(
-                            'https://shop.upsu.net/cdn/shop/files/upsu_300x300.png?v=1614735854',
+    return Material(
+      elevation: 1.0,
+      child: Container(
+        height: 100,
+        color: Colors.white,
+        child: Column(
+          children: [
+            // Top banner
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              color: const Color(0xFF4d2963),
+              child: const Text(
+                'UNIVERSITY OF PORTSMOUTH SHOP',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+            ),
+            // Main header
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        navigateToHome(context);
+                      },
+                      child: Image.network(
+                        'https://shop.upsu.net/cdn/shop/files/upsu_300x300.png?v=1614735854',
+                        height: 18,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Container(
+                            color: Colors.grey[300],
+                            width: 18,
                             height: 18,
-                            fit: BoxFit.cover,
-                            errorBuilder: (context, error, stackTrace) {
-                              return Container(
-                                color: Colors.grey[300],
-                                width: 18,
-                                height: 18,
-                                child: const Center(
-                                  child: Icon(Icons.image_not_supported,
-                                      color: Colors.grey),
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                        const Spacer(),
-                        ConstrainedBox(
-                          constraints: const BoxConstraints(maxWidth: 600),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              AppBarButton(
-                                icon: Icons.search,
-                                onPressed: placeholderCallbackForButtons,
-                              ),
-                              AppBarButton(
-                                icon: Icons.person_outline,
-                                onPressed: placeholderCallbackForButtons,
-                              ),
-                              AppBarButton(
-                                icon: Icons.shopping_bag_outlined,
-                                onPressed: placeholderCallbackForButtons,
-                              ),
-                              AppBarButton(
-                                icon: Icons.menu,
-                                onPressed: _toggleMenu,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+                            child: const Center(
+                              child: Icon(Icons.image_not_supported,
+                                  color: Colors.grey),
+                            ),
+                          );
+                        },
+                      ),
                     ),
-                  ),
+                    const Spacer(),
+                    ConstrainedBox(
+                      constraints: const BoxConstraints(maxWidth: 600),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          AppBarButton(
+                            icon: Icons.search,
+                            onPressed: placeholderCallbackForButtons,
+                          ),
+                          AppBarButton(
+                            icon: Icons.person_outline,
+                            onPressed: placeholderCallbackForButtons,
+                          ),
+                          AppBarButton(
+                            icon: Icons.shopping_bag_outlined,
+                            onPressed: placeholderCallbackForButtons,
+                          ),
+                          AppBarButton(
+                            icon: Icons.menu,
+                            onPressed: widget.onMenuPressed,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
-
-        // The dropdown menu, positioned below the AppBar.
-        if (_isMenuOpen)
-          const Positioned(
-            top: 100, // Position it just below the AppBar
-            left: 0,
-            right: 0,
-            child: Material(
-              elevation: 1.0, // Give it a slight shadow to stand out
-              child: MobileMenu(),
-            ),
-          ),
-      ],
+      ),
     );
   }
 }
