c4b1be032eaa781b6f8eb27c8a956b5b3567beb1
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-11-28 20:51:48 +0000

    Add mock implementation of CartService for page wrapper tests

--- test/widgets/page_wrapper_test.dart
+++ test/widgets/page_wrapper_test.dart
@@ -1,7 +1,40 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:union_shop/models/cart_model.dart';
+import 'package:union_shop/models/product_model.dart';
+import 'package:union_shop/services/cart_service.dart';
+import 'package:union_shop/widgets/app_bar.dart';
+import 'package:union_shop/widgets/footer.dart';
+import 'package:union_shop/widgets/mobile_menu.dart';
+import 'package:union_shop/widgets/page_wrapper.dart';
 
-void main() {
-  test('page wrapper test', () {
-    // TODO: Implement test
-  });
+class MockCartService extends ChangeNotifier implements CartService {
+  @override
+  List<CartItem> get items => [];
+  @override
+  int get totalItems => 0;
+  @override
+  double get totalPrice => 0.0;
+
+  @override
+  void addToCart(Product product,
+      {String? size,
+      String? color,
+      String? customText,
+      String? customColorName,
+      int quantity = 1}) {}
+
+  @override
+  void clearCart() {}
+
+  @override
+  void removeFromCart(String productId, String? size, String? color,
+      {String? customText, String? customColorName}) {}
+
+  @override
+  void updateQuantity(
+      String productId, int quantity, String? size, String? color,
+      {String? customText, String? customColorName}) {}
 }
+
