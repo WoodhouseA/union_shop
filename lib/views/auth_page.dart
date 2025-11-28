83b9750ed24e505952065df226e8748923659b42
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-11-28 15:56:22 +0000

    Add AuthPage widget to manage login and signup screens with toggle functionality

---
+++ lib/views/auth_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:union_shop/widgets/login_form.dart';
+import 'package:union_shop/widgets/signup_form.dart';
+
+class AuthPage extends StatefulWidget {
+  const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool _showLoginPage = true;
+
+  void _toggleScreens() {
+    setState(() {
+      _showLoginPage = !_showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SingleChildScrollView(
+        child: _showLoginPage
+            ? LoginForm(onToggle: _toggleScreens)
+            : SignupForm(onToggle: _toggleScreens),
+      ),
+    );
+  }
+}
