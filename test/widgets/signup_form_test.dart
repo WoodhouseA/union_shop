e0bf2dc7887a0f6fd81824c31c700d2bab1e71d9
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-11-28 21:02:31 +0000

    Add tests for SignupForm to verify display of all fields

--- test/widgets/signup_form_test.dart
+++ test/widgets/signup_form_test.dart
@@ -1,7 +1,26 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:union_shop/widgets/signup_form.dart';
 
 void main() {
-  test('signup form test', () {
-    // TODO: Implement test
+  Widget createTestWidget({VoidCallback? onToggle}) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SignupForm(
+          onToggle: onToggle ?? () {},
+        ),
+      ),
+    );
+  }
+
+  testWidgets('SignupForm displays all fields', (WidgetTester tester) async {
+    await tester.pumpWidget(createTestWidget());
+
+    // There are two "Sign Up" texts: one in the title and one in the button
+    expect(find.text('Sign Up'), findsNWidgets(2));
+    expect(find.text('Email'), findsOneWidget);
+    expect(find.text('Password'), findsOneWidget);
+    expect(find.byType(ElevatedButton), findsOneWidget);
+    expect(find.text('Already have an account? Login'), findsOneWidget);
   });
 }
