62db988b37b212adbcd75d5f044550e2783eb2c4
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-11-28 20:58:24 +0000

    Add tests for ProductCard to verify display of product information

--- test/widgets/product_card_test.dart
+++ test/widgets/product_card_test.dart
@@ -1,7 +1,37 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:union_shop/models/product_model.dart';
+import 'package:union_shop/widgets/product_card.dart';
 
 void main() {
-  test('product card test', () {
-    // TODO: Implement test
+  Widget createTestWidget(Product product) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SizedBox(
+          width: 300, // Increased width to prevent overflow
+          height: 300,
+          child: ProductCard(product: product),
+        ),
+      ),
+    );
+  }
+
+  testWidgets('ProductCard displays regular product info', (WidgetTester tester) async {
+    final product = Product(
+      id: '1',
+      collectionId: 'col1',
+      name: 'Test Product',
+      price: 10.0,
+      onSale: false,
+      imageUrl: 'http://example.com/image.jpg',
+      sizes: [],
+      colors: [],
+    );
+
+    await tester.pumpWidget(createTestWidget(product));
+
+    expect(find.text('Test Product'), findsOneWidget);
+    expect(find.text('£10.00'), findsOneWidget);
+    expect(find.byType(Image), findsOneWidget);
   });
 }
