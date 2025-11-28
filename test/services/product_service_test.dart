52769ee71cdeb046c5be84369234e7679c0e1599
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-11-28 19:47:01 +0000

    Add tests for ProductService including mock data setup

--- test/services/product_service_test.dart
+++ test/services/product_service_test.dart
@@ -1,7 +1,79 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:union_shop/services/product_service.dart';
 
 void main() {
-  test('product service test', () {
-    // TODO: Implement test
+  TestWidgetsFlutterBinding.ensureInitialized();
+  late ProductService productService;
+
+  final mockProducts = [
+    {
+      "id": "1",
+      "collectionId": "col1",
+      "name": "Product 1",
+      "price": 10.0,
+      "onSale": false,
+      "imageUrl": "url1",
+      "sizes": [],
+      "colors": []
+    },
+    {
+      "id": "2",
+      "collectionId": "col2",
+      "name": "Product 2",
+      "price": 20.0,
+      "onSale": true,
+      "salePrice": 15.0,
+      "imageUrl": "url2",
+      "sizes": [],
+      "colors": []
+    }
+  ];
+
+  setUp(() {
+    productService = ProductService();
+
+    // Mock the asset loading
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMessageHandler(
+      'flutter/assets',
+      (ByteData? message) async {
+        final String jsonStr = json.encode(mockProducts);
+        final Uint8List utf8Bytes = utf8.encode(jsonStr);
+        return ByteData.view(utf8Bytes.buffer);
+      },
+    );
+  });
+
+  tearDown(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMessageHandler(
+      'flutter/assets',
+      null,
+    );
+  });
+
+  test('getAllProducts returns all products', () async {
+    final products = await productService.getAllProducts();
+    expect(products.length, 2);
+    expect(products[0].name, 'Product 1');
+  });
+
+  test('getProductsByCollection returns filtered products', () async {
+    final products = await productService.getProductsByCollection('col1');
+    expect(products.length, 1);
+    expect(products[0].id, '1');
+  });
+
+  test('getSaleProducts returns only sale products', () async {
+    final products = await productService.getSaleProducts();
+    expect(products.length, 1);
+    expect(products[0].id, '2');
+  });
+
+  test('getProductById returns correct product', () async {
+    final product = await productService.getProductById('1');
+    expect(product.name, 'Product 1');
   });
 }
