5c4be66ad360f95ebba74d2c6d7393a69100d6da
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-11-28 20:25:20 +0000

    Add MockCartService implementation for app bar tests

--- test/widgets/app_bar_test.dart
+++ test/widgets/app_bar_test.dart
@@ -1,7 +1,37 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:union_shop/models/cart_model.dart';
+import 'package:union_shop/models/product_model.dart';
+import 'package:union_shop/services/cart_service.dart';
+import 'package:union_shop/widgets/app_bar.dart';
 
-void main() {
-  test('app bar test', () {
-    // TODO: Implement test
-  });
+class MockCartService extends ChangeNotifier implements CartService {
+  @override
+  List<CartItem> get items => [];
+  @override
+  int get totalItems => 0;
+  @override
+  double get totalPrice => 0.0;
+
+  @override
+  void addToCart(Product product,
+      {String? size,
+      String? color,
+      String? customText,
+      String? customColorName,
+      int quantity = 1}) {}
+
+  @override
+  void clearCart() {}
+
+  @override
+  void removeFromCart(String productId, String? size, String? color,
+      {String? customText, String? customColorName}) {}
+
+  @override
+  void updateQuantity(
+      String productId, int quantity, String? size, String? color,
+      {String? customText, String? customColorName}) {}
 }
+
