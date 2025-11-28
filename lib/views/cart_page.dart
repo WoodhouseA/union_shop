1afa9893c1c80eae294c61a7ded706e21dc73080
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-11-28 17:01:51 +0000

    Refactor cart page to use context.pop() for navigation

--- lib/views/cart_page.dart
+++ lib/views/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:union_shop/services/cart_service.dart';
 
@@ -134,7 +135,7 @@ class CartPage extends StatelessWidget {
                               TextButton(
                                 onPressed: () {
                                   cartService.clearCart();
-                                  Navigator.of(context).pop();
+                                  context.pop();
                                 },
                                 child: const Text('OK'),
                               ),
