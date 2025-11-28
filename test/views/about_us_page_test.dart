e3de680bd4508bf2814ad1e7f4eebdefb493fbd9
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-11-28 21:35:17 +0000

    Add widget test for AboutUsPage to verify content and layout

--- test/views/about_us_page_test.dart
+++ test/views/about_us_page_test.dart
@@ -1,7 +1,24 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:union_shop/views/about_us_page.dart';
 
 void main() {
-  test('about us page test', () {
-    // TODO: Implement test
+  testWidgets('AboutUsPage displays correct content', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: AboutUsPage()));
+
+    // Verify title
+    expect(find.text('Welcome to the Union Shop!'), findsOneWidget);
+
+    // Verify body text parts
+    expect(find.textContaining('We’re dedicated to giving you the very best University branded products'), findsOneWidget);
+    expect(find.textContaining('All online purchases are available for delivery or instore collection!'), findsOneWidget);
+    expect(find.textContaining('hello@upsu.net'), findsOneWidget);
+    
+    // Verify footer
+    expect(find.text('Happy shopping!'), findsOneWidget);
+    expect(find.text('The Union Shop & Reception Team'), findsOneWidget);
+
+    // Verify scroll view
+    expect(find.byType(SingleChildScrollView), findsOneWidget);
   });
 }
