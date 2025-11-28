59f3f175da29b5cc90f6d4dd072784d0c592aad1
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-11-28 11:46:14 +0000

    Add size attribute to CartItem model

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -3,9 +3,11 @@ import 'package:union_shop/models/product_model.dart';
 class CartItem {
   final Product product;
   int quantity;
+  final String? size;
 
   CartItem({
     required this.product,
     this.quantity = 1,
+    this.size,
   });
 }
