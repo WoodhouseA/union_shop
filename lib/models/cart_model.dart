2f966188960f362c5627f98da74ba61cc2b563ce
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-11-28 16:20:48 +0000

    Add custom attributes to CartItem model for enhanced personalization

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -5,11 +5,17 @@ class CartItem {
   int quantity;
   final String? size;
   final String? color;
+  final String? customText;
+  final String? customFont;
+  final String? customColorName;
 
   CartItem({
     required this.product,
     this.quantity = 1,
     this.size,
     this.color,
+    this.customText,
+    this.customFont,
+    this.customColorName,
   });
 }
