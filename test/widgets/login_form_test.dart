4e687510c03c61feb88247afb1fe6939923724c6
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-11-28 20:42:31 +0000

    Add tests for LoginForm to verify field visibility and layout

--- test/widgets/login_form_test.dart
+++ test/widgets/login_form_test.dart
@@ -1,7 +1,26 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:union_shop/widgets/login_form.dart';
 
 void main() {
-  test('login form test', () {
-    // TODO: Implement test
+  Widget createTestWidget({VoidCallback? onToggle}) {
+    return MaterialApp(
+      home: Scaffold(
+        body: LoginForm(
+          onToggle: onToggle ?? () {},
+        ),
+      ),
+    );
+  }
+
+  testWidgets('LoginForm displays all fields', (WidgetTester tester) async {
+    await tester.pumpWidget(createTestWidget());
+
+    // There are two "Login" texts: one in the title and one in the button
+    expect(find.text('Login'), findsNWidgets(2));
+    expect(find.text('Email'), findsOneWidget);
+    expect(find.text('Password'), findsOneWidget);
+    expect(find.byType(ElevatedButton), findsOneWidget);
+    expect(find.text('Don\'t have an account? Sign up'), findsOneWidget);
   });
 }
